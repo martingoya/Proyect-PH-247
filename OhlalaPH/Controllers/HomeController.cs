756d6f2923a3146fd5b44a266c7f5453ef82bbcf
tincho592 <martin.goya@hotmail.com>
2016-04-04 17:09:50 -0300

    ADD Contact Form and Model

--- OhlalaPH/Controllers/HomeController.cs
+++ OhlalaPH/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using OhlalaPH.Models;
+using System.Text;
 
 namespace OhlalaPH.Controllers
 {
@@ -26,6 +29,42 @@ namespace OhlalaPH.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactModels c)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msg = new MailMessage();
+                    SmtpClient smtp = new SmtpClient();
+                    MailAddress from = new MailAddress(c.Email.ToString());
+                    StringBuilder sb = new StringBuilder();
+                    msg.To.Add("tincho_592@hotmail.com");
+                    msg.Subject = "Contacto";
+                    msg.IsBodyHtml = false;
+                    smtp.Host = "mail.yourdomain.com";
+                    smtp.Port = 25;
+                    sb.Append("Nombre: " + c.Name);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Mail: " + c.Email);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Teléfono: " + c.Tel);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Consulta: " + c.Comment);
+                    msg.Body = sb.ToString();
+                    smtp.Send(msg);
+                    msg.Dispose();
+                    return View("Success");
+                }
+                catch (Exception)
+                {
+                    return View("Error");
+                }
+            }
             return View();
         }
     }
---
+++ OhlalaPH/Models/ContactModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace OhlalaPH.Models
+{
+    public class ContactModels
+    {
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Tel { get; set; }
+        public string Comment { get; set; }
+    }
+}
